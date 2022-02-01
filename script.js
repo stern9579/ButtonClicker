1dfcf68d48d22019cf941a1adbc362bd60119fdd
Andrew Stern <andrewdstern16@gmail.com>
2022-01-31 17:26:56 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function login(element) {
+    element.innerText = "Logout"
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function notify() {
+    alert("Ninja was liked");
+}
+
+
+
+
+// function example(element) {
+//     console.log("element clicked", element);
+// }
+
+// function turnOff(element) {
+//         element.innerText = "Off";
+// }
+
+// function hide(element) {
+//     element.remove();
+// }
